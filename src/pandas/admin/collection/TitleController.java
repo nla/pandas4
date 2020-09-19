2deae17f44765e57c27dcdcfb1016d1a5908a51a
Alex Osborne <aosborne@nla.gov.au>
2020-09-19 11:40:06 +0900

    Move Facet to search package

--- src/pandas/admin/collection/TitleController.java
+++ src/pandas/admin/collection/TitleController.java
@@ -1,9 +1,5 @@
 package pandas.admin.collection;
 
-import org.hibernate.search.engine.search.aggregation.AggregationKey;
-import org.hibernate.search.engine.search.predicate.dsl.BooleanPredicateClausesStep;
-import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
-import org.hibernate.search.engine.search.query.SearchResult;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.springframework.data.domain.Pageable;
@@ -24,15 +20,14 @@ import pandas.admin.gather.GatherMethod;
 import pandas.admin.gather.GatherMethodRepository;
 import pandas.admin.gather.GatherSchedule;
 import pandas.admin.gather.GatherScheduleRepository;
-import pandas.admin.search.FacetEntry;
+import pandas.admin.search.Facet;
 import pandas.admin.search.FacetResults;
 import pandas.admin.search.SearchResults;
 
 import javax.persistence.EntityManager;
-import java.util.*;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.List;
 
-import static java.util.Comparator.*;
 import static org.hibernate.search.engine.search.common.BooleanOperator.AND;
 import static pandas.admin.search.SearchUtils.mustMatchAny;
 
@@ -61,58 +56,6 @@ public class TitleController {
         return new RedirectView(config.managementDirectActionUrl("titleView?id=" + id));
     }
 
-    private static class Facet<T> {
-        private final String name;
-        private final String queryParam;
-        private final String indexField;
-        private final AggregationKey<Map<Long, Long>> key;
-        private final Function<Iterable<Long>, Iterable<T>> lookupFunction;
-        private final Function<T, Long> idFunction;
-        private final Function<T, String> nameFunction;
-
-        private Facet(String name, String queryParam, String indexField,
-                      Function<Iterable<Long>, Iterable<T>> lookupFunction,
-                      Function<T, Long> idFunction,
-                      Function<T, String> nameFunction) {
-            this.name = name;
-            this.queryParam = queryParam;
-            this.indexField = indexField;
-            this.key = AggregationKey.of(name);
-            this.lookupFunction = lookupFunction;
-            this.idFunction = idFunction;
-            this.nameFunction = nameFunction;
-        }
-
-        Set<Long> parseParam(MultiValueMap<String, String> queryParams) {
-            List<String> values = queryParams.get(queryParam);
-            if (values == null || values.isEmpty()) return Collections.emptySet();
-            Set<Long> ids = new HashSet<>();
-            for (String value : values) {
-                ids.add(Long.parseLong(value));
-            }
-            return ids;
-        }
-
-        void mustMatch(SearchPredicateFactory predicateFactory, BooleanPredicateClausesStep<?> bool, MultiValueMap<String, String> queryParams) {
-            mustMatchAny(predicateFactory, bool, indexField, parseParam(queryParams));
-        }
-
-        FacetResults results(MultiValueMap<String, String> queryParams, SearchResult<?> result) {
-            var counts = result.aggregation(key);
-            Set<Long> activeIds = parseParam(queryParams);
-            Set<Long> idSet = new HashSet<>();
-            idSet.addAll(counts.keySet());
-            idSet.addAll(activeIds);
-            List<FacetEntry> entries = new ArrayList<>();
-            for (T entity : lookupFunction.apply(idSet)) {
-                Long id = idFunction.apply(entity);
-                entries.add(new FacetEntry(id, nameFunction.apply(entity), counts.get(id), activeIds.contains(id)));
-            }
-            entries.sort(comparing(FacetEntry::isActive).thenComparing(FacetEntry::getCount, nullsFirst(naturalOrder())).reversed());
-            return new FacetResults(queryParam, name, entries);
-        }
-    }
-
     @GetMapping("/titles")
     public String search(@RequestParam(name = "q", required = false) String rawQ,
                          @RequestParam(name = "collection", defaultValue = "") List<Long> collectionIds,
@@ -125,9 +68,10 @@ public class TitleController {
         var search = session.search(Title.class)
                 .where(f -> f.bool(b -> {
                     b.must(f.matchAll());
-                    if (q != null) b.must(f.simpleQueryString().fields("name", "titleUrl", "seedUrl", "gather.notes").matching(q).defaultOperator(AND));
+                    if (q != null)
+                        b.must(f.simpleQueryString().fields("name", "titleUrl", "seedUrl", "gather.notes").matching(q).defaultOperator(AND));
                     mustMatchAny(f, b, "collections.id", collectionIds);
-                    for (Facet<?> filter: facets) {
+                    for (Facet<?> filter : facets) {
                         filter.mustMatch(f, b, queryParams);
                     }
                 }))
---
+++ src/pandas/admin/search/Facet.java
@@ -0,0 +1,65 @@
+package pandas.admin.search;
+
+import org.hibernate.search.engine.search.aggregation.AggregationKey;
+import org.hibernate.search.engine.search.predicate.dsl.BooleanPredicateClausesStep;
+import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
+import org.hibernate.search.engine.search.query.SearchResult;
+import org.springframework.util.MultiValueMap;
+
+import java.util.*;
+import java.util.function.Function;
+
+import static java.util.Comparator.*;
+import static pandas.admin.search.SearchUtils.mustMatchAny;
+
+public class Facet<T> {
+    private final String name;
+    public final String queryParam;
+    public final String indexField;
+    public final AggregationKey<Map<Long, Long>> key;
+    private final Function<Iterable<Long>, Iterable<T>> lookupFunction;
+    private final Function<T, Long> idFunction;
+    private final Function<T, String> nameFunction;
+
+    public Facet(String name, String queryParam, String indexField,
+                 Function<Iterable<Long>, Iterable<T>> lookupFunction,
+                 Function<T, Long> idFunction,
+                 Function<T, String> nameFunction) {
+        this.name = name;
+        this.queryParam = queryParam;
+        this.indexField = indexField;
+        this.key = AggregationKey.of(name);
+        this.lookupFunction = lookupFunction;
+        this.idFunction = idFunction;
+        this.nameFunction = nameFunction;
+    }
+
+    private Set<Long> parseParam(MultiValueMap<String, String> queryParams) {
+        List<String> values = queryParams.get(queryParam);
+        if (values == null || values.isEmpty()) return Collections.emptySet();
+        Set<Long> ids = new HashSet<>();
+        for (String value : values) {
+            ids.add(Long.parseLong(value));
+        }
+        return ids;
+    }
+
+    public void mustMatch(SearchPredicateFactory predicateFactory, BooleanPredicateClausesStep<?> bool, MultiValueMap<String, String> queryParams) {
+        mustMatchAny(predicateFactory, bool, indexField, parseParam(queryParams));
+    }
+
+    public FacetResults results(MultiValueMap<String, String> queryParams, SearchResult<?> result) {
+        var counts = result.aggregation(key);
+        Set<Long> activeIds = parseParam(queryParams);
+        Set<Long> idSet = new HashSet<>();
+        idSet.addAll(counts.keySet());
+        idSet.addAll(activeIds);
+        List<FacetEntry> entries = new ArrayList<>();
+        for (T entity : lookupFunction.apply(idSet)) {
+            Long id = idFunction.apply(entity);
+            entries.add(new FacetEntry(id, nameFunction.apply(entity), counts.get(id), activeIds.contains(id)));
+        }
+        entries.sort(comparing(FacetEntry::isActive).thenComparing(FacetEntry::getCount, nullsFirst(naturalOrder())).reversed());
+        return new FacetResults(queryParam, name, entries);
+    }
+}
