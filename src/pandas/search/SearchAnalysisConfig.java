5b487f0e862320d48c874969aecaca8c5c4be455
Alex Osborne <aosborne@nla.gov.au>
2020-10-08 13:53:37 +0900

    Enable stemming in english analyser

--- src/pandas/search/SearchAnalysisConfig.java
+++ src/pandas/search/SearchAnalysisConfig.java
@@ -1,7 +1,10 @@
 package pandas.search;
 
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.snowball.SnowballPorterFilterFactory;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
 
@@ -9,7 +12,12 @@ public class SearchAnalysisConfig implements LuceneAnalysisConfigurer {
     @Override
     public void configure(LuceneAnalysisConfigurationContext context) {
         context.analyzer("standard").instance(new StandardAnalyzer());
-        context.analyzer( "english" ).instance(new StandardAnalyzer(EnglishAnalyzer.ENGLISH_STOP_WORDS_SET));
         context.analyzer( "url" ).instance(new StandardAnalyzer());
+        context.analyzer("english").custom()
+                .tokenizer(StandardTokenizerFactory.class)
+                .tokenFilter(LowerCaseFilterFactory.class)
+                .tokenFilter(SnowballPorterFilterFactory.class).param("language", "English")
+                .tokenFilter(ASCIIFoldingFilterFactory.class);
+
     }
 }
