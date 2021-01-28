0db7947dd03bc66aa548f292c0ef4c09ad3f2534
Alex Osborne <aosborne@nla.gov.au>
2021-01-28 13:22:59 +0900

    Add a 10 min maximum lifetime for browser processes
    Mitigates any slow memory leaks

--- src/pandas/render/BrowserPool.java
+++ src/pandas/render/BrowserPool.java
@@ -2,17 +2,31 @@ package pandas.render;
 
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultEvictionPolicy;
 import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.apache.commons.pool2.impl.EvictionConfig;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 
 public class BrowserPool extends GenericObjectPool<Browser> {
+    long maxLifetime = 10L * 60L * 1000L; // kill browser after 10 minutes
+
     public BrowserPool() {
         super(new Factory());
         setTestOnBorrow(true);
+        setTimeBetweenEvictionRunsMillis(30000);
         setMinEvictableIdleTimeMillis(30000);
+        setEvictionPolicy(new DefaultEvictionPolicy<>() {
+            @Override
+            public boolean evict(EvictionConfig config, PooledObject<Browser> underTest, int idleCount) {
+                if (System.currentTimeMillis() - underTest.getCreateTime() > maxLifetime) {
+                    return true;
+                }
+                return super.evict(config, underTest, idleCount);
+            }
+        });
     }
 
     private static class Factory extends BasePooledObjectFactory<Browser> {
