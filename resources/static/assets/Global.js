925275c60fda97f6ecde643f549065b60a3c132a
Alex Osborne <aosborne@nla.gov.au>
2021-02-01 10:04:15 +0900

    Add icons to buttons and migrate some of the search CSS

--- resources/static/assets/Global.js
+++ resources/static/assets/Global.js
@@ -1,10 +1,14 @@
 document.addEventListener('keyup', ev => {
     if (ev.altKey || ev.ctrlKey || ev.metaKey || ev.shiftKey ||
-        ev.target.tagName === 'INPUT' || ev.target.tagName === 'SELECT' || ev.target.tagName === 'TEXTAREA') {
+        (ev.key !== "Escape" && (ev.target.tagName === 'INPUT' || ev.target.tagName === 'SELECT' || ev.target.tagName === 'TEXTAREA'))) {
         return;
     }
     const el = document.querySelector("[data-keybind='" + CSS.escape(ev.key) + "']");
     if (el) {
-        el.click();
+        if (el.tagName === 'A' || el.tagName === 'BUTTON') {
+            el.click();
+        } else {
+            el.focus();
+        }
     }
 });
