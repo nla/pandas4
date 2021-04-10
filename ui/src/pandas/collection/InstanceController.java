5405038f6040234b8d73261dcc0d52f68b6f4af6
Alex Osborne <aosborne@nla.gov.au>
2021-04-10 10:52:14 +0900

    Add placeholder thumbnail when instance is not in archived state

--- ui/src/pandas/collection/InstanceController.java
+++ ui/src/pandas/collection/InstanceController.java
@@ -70,9 +70,19 @@ public class InstanceController {
 
     @GetMapping("/instances/{id}/thumbnail")
     public ResponseEntity<byte[]> getThumbnail(@PathVariable("id") Instance instance) {
+        // icons from material.io/icons
         if (instance.getState().getName().equals(State.DELETED) || instance.getState().getName().equals(State.DELETING)) {
             return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/svg+xml"))
                     .body("<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24px\" viewBox=\"0 0 24 24\" width=\"24px\" fill=\"#999999\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M6 21h12V7H6v14zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z\"/></svg>".getBytes(UTF_8));
+        } else if (instance.getState().getName().equals(State.GATHERED)) {
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/svg+xml"))
+                    .body("<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24px\" viewBox=\"0 0 24 24\" width=\"24px\" fill=\"#999999\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M5 18h14v2H5v-2zm4.6-2.7L5 10.7l2-1.9 2.6 2.6L17 4l2 2-9.4 9.3z\"/></svg>".getBytes(UTF_8));
+        } else if (instance.getState().getName().equals(State.FAILED)) {
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/svg+xml"))
+                    .body("<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24px\" viewBox=\"0 0 24 24\" width=\"24px\" fill=\"#999999\"><path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z\"/></svg>".getBytes(UTF_8));
+        } else if (!instance.getState().getName().equals(State.ARCHIVED)) {
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/svg+xml"))
+                    .body("<svg xmlns=\"http://www.w3.org/2000/svg\" enable-background=\"new 0 0 24 24\" height=\"24px\" viewBox=\"0 0 24 24\" width=\"24px\" fill=\"#999999\"><g><rect fill=\"none\" height=\"24\" width=\"24\"/></g><g><path d=\"M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10C22,6.48,17.52,2,12,2z M7,13.5c-0.83,0-1.5-0.67-1.5-1.5 c0-0.83,0.67-1.5,1.5-1.5s1.5,0.67,1.5,1.5C8.5,12.83,7.83,13.5,7,13.5z M12,13.5c-0.83,0-1.5-0.67-1.5-1.5 c0-0.83,0.67-1.5,1.5-1.5s1.5,0.67,1.5,1.5C13.5,12.83,12.83,13.5,12,13.5z M17,13.5c-0.83,0-1.5-0.67-1.5-1.5 c0-0.83,0.67-1.5,1.5-1.5s1.5,0.67,1.5,1.5C18.5,12.83,17.83,13.5,17,13.5z\"/></g></svg>".getBytes(UTF_8));
         }
 
         var thumbnail = instance.getThumbnail();
