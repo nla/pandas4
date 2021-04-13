d3552d73cc72bee95b1ba570ff397734af1f38cb
Alex Osborne <aosborne@nla.gov.au>
2021-04-13 09:22:33 +0900

    Make health check available without auth

--- gatherer/src/pandas/PandasGatherer.java
+++ gatherer/src/pandas/PandasGatherer.java
@@ -2,10 +2,25 @@ package pandas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class PandasGatherer {
     public static void main(String[] args) {
         SpringApplication.run(PandasGatherer.class, args);
+    }
+
+    @EnableWebFluxSecurity
+    static class SecurityConfig {
+        @Bean
+        public SecurityWebFilterChain securitygWebFilterChain(
+                ServerHttpSecurity http) {
+            return http.authorizeExchange()
+                    .anyExchange().permitAll()
+                    .and().build();
+        }
     }
 }
