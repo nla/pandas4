a96a76a05bfa02c1e92a2dd4f3a5cc26c611bc5d
Alex Osborne <aosborne@nla.gov.au>
2021-05-04 12:43:04 +0900

    ProcessInstance: Add keybindings :o)

--- ui/resources/static/assets/Global.js
+++ ui/resources/static/assets/Global.js
@@ -1,9 +1,13 @@
 document.addEventListener('keyup', ev => {
-    if (ev.altKey || ev.ctrlKey || ev.metaKey || ev.shiftKey ||
+    if (ev.metaKey ||
         (ev.key !== "Escape" && (ev.target.tagName === 'INPUT' || ev.target.tagName === 'SELECT' || ev.target.tagName === 'TEXTAREA'))) {
         return;
     }
-    const el = document.querySelector("[data-keybind='" + CSS.escape(ev.key) + "']");
+    let modifiers = "";
+    if (ev.ctrlKey) modifiers += "Ctrl+";
+    if (ev.altKey) modifiers += "Alt+";
+    if (ev.shiftKey) modifiers += "Shift+";
+    const el = document.querySelector("[data-keybind='" + CSS.escape(modifiers + ev.key) + "']");
     if (el) {
         if (el.tagName === 'A' || el.tagName === 'BUTTON') {
             el.click();
