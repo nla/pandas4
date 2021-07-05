cd38e3e7678a546fd7352d9e165fac77536b198b
Alex Osborne <aosborne@nla.gov.au>
2021-07-05 14:29:31 +0900

    Gatherer: Improve global replace error response

--- gatherer/src/pandas/gatherer/scripter/ScripterController.java
+++ gatherer/src/pandas/gatherer/scripter/ScripterController.java
@@ -1,13 +1,15 @@
 package pandas.gatherer.scripter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 import pandas.gather.Instance;
 import pandas.gather.InstanceRepository;
 import pandas.gatherer.core.WorkingArea;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Controller
 public class ScripterController {
@@ -19,10 +21,17 @@ public class ScripterController {
         this.workingArea = workingArea;
     }
 
+    @GetMapping("/replaceAllInInstance")
+    @ResponseBody
+    public String getReplaceAllInInstance() {
+        return "POST expected";
+    }
+
     @PostMapping("/replaceAllInInstance")
+    @ResponseBody
     public String replaceAllInInstance(GlobalReplaceRequest request) {
         Instance instance = instanceRepository.findById(request.instanceId)
-                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "No such instance"));
+                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, "No such instance"));
         request.pi = instance.getPi();
         request.root = workingArea.getInstanceDir(instance.getPi(), instance.getDateString());
         return "global replacement queued.";
