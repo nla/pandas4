452d0fc4b25a90013f24d3788ca7db3a3dee1776
Alex Osborne <aosborne@nla.gov.au>
2021-07-29 12:23:09 +0900

    Bookmarklet: Add bookmarklet that annotates Google results

---
+++ ui/resources/bookmarklets/google.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('.pandas-div').forEach(div => div.parentNode.removeChild(div));
+Array.from(document.querySelectorAll('#search a > h3')).forEach(h3 => {
+    let pandasUrl = "https://pandas.nla.gov.au/admin";
+    let link = h3.parentNode;
+    fetch(pandasUrl + "/titles/check?url=" + encodeURIComponent(link.href))
+        .then(r => r.json())
+        .then(titles => {
+            let div = document.createElement('div');
+            div.style.cssText = "position: absolute";
+            if (titles.length === 0) {
+                let a = document.createElement("a");
+                a.innerText = "[New Title]";
+                a.href = pandasUrl + "/titles/new?url=" + encodeURIComponent(link.href);
+                div.appendChild(a);
+            } else {
+                titles.forEach(title => {
+                    let a = document.createElement("a");
+                    a.innerText = "nla.arc-" + title.pi;
+                    a.title = title.name;
+                    a.href = pandasUrl + "/titles/" + title.id;
+                    div.appendChild(a);
+                    div.appendChild(document.createElement("br"));
+                })
+            }
+            let div2 = document.createElement('div');
+            div2.classList.add('pandas-div');
+            div2.style.cssText = "position: relative; left: -120px; width: 112px; top: 0px";
+            div2.innerHTML = '';
+            div2.appendChild(div);
+            let container = link.parentNode.parentNode;
+            container.insertBefore(div2, container.childNodes[0]);
+        });
+})
---
+++ ui/src/pandas/discovery/BookmarkletController.java
@@ -0,0 +1,27 @@
+package pandas.discovery;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Controller
+public class BookmarkletController {
+    @GetMapping(value = "/bookmarklet")
+    public void get(Model model, HttpServletRequest request) throws IOException {
+        try (InputStream stream = BookmarkletController.class.getResourceAsStream("/bookmarklets/google.js")) {
+            String bookmarklet = new String(stream.readAllBytes(), UTF_8);
+            String baseUrl = ServletUriComponentsBuilder.fromRequest(request).replacePath("").build().toString();
+            bookmarklet = bookmarklet.replace("https://pandas.nla.gov.au/admin", baseUrl);
+            bookmarklet = bookmarklet.replace("\n", "").replaceAll("  +", " ");
+            model.addAttribute("bookmarklet", "javascript:" + UriUtils.encode(bookmarklet, UTF_8));
+        }
+    }
+}
