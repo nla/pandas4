6e24cb3aee2cfbcca95e1b0ba66129780124a482
Alex Osborne <aosborne@nla.gov.au>
2022-03-28 01:17:03 +0900

    UI: Have Hibernate populate db schema if not using Flyway

---
+++ ui/src/pandas/core/DatabasePropertiesListener.java
@@ -0,0 +1,28 @@
+package pandas.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+
+import java.util.Properties;
+
+public class DatabasePropertiesListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+    private final Logger log = LoggerFactory.getLogger(DatabasePropertiesListener.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        ConfigurableEnvironment environment = event.getEnvironment();
+        if (environment.getProperty("spring.datasource.url", "").startsWith("jdbc:oracle:")) {
+            return; // Use Flyway
+        }
+        log.info("Not using Flyway so allowing Hibernate to manage the database schema");
+        Properties props = new Properties();
+        props.put("spring.jpa.hibernate.ddl-auto", "update");
+        props.put("spring.session.jdbc.initialize-schema", "always");
+        props.put("spring.datasource.initialization-mode", "always");
+        environment.getPropertySources().addFirst(new PropertiesPropertySource("dynamicDbProps", props));
+    }
+}
