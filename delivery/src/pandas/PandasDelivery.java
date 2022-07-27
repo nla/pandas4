d856d9e6b234e672af8d7d9c727a74f7c1fb1db3
Alex Osborne <aosborne@nla.gov.au>
2022-07-27 09:45:23 +0900

    delivery: Enable scheduling so the search indexes are updated

--- delivery/src/pandas/PandasDelivery.java
+++ delivery/src/pandas/PandasDelivery.java
@@ -5,8 +5,10 @@ import org.owasp.html.Sanitizers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PandasDelivery {
     public static void main(String[] args) {
         SpringApplication.run(PandasDelivery.class, args);
