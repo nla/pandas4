5e07ddf7796828d3033112c44c0d6853d2293eda
Alex Osborne <aosborne@nla.gov.au>
2023-01-06 13:34:10 +0900

    Add bing support to bookmarklet

---
+++ ui/resources/bookmarklets/bookmarklet.js
@@ -0,0 +1,48 @@
+{
+    document.querySelectorAll('.pandas-div').forEach(div => div.parentNode.removeChild(div));
+    let selector;
+    if (location.host === 'www.bing.com') {
+        selector = '#b_results > li cite';
+    } else {
+        selector = '#search a > h3';
+    }
+    Array.from(document.querySelectorAll(selector)).forEach(link => {
+        let pandasUrl = "https://pandas.nla.gov.au/admin";
+        let url;
+        if (location.host === 'www.bing.com') {
+            url = link.innerText;
+        } else {
+            link = link.parentNode;
+            url = link.href;
+        }
+        fetch(pandasUrl + "/titles/check?url=" + encodeURIComponent(url))
+            .then(r => r.json())
+            .then(titles => {
+                let div = document.createElement('div');
+                if (titles.length === 0) {
+                    let a = document.createElement("a");
+                    a.innerText = "[New Title]";
+                    a.href = pandasUrl + "/titles/new?url=" + encodeURIComponent(url);
+                    div.appendChild(a);
+                } else {
+                    titles.forEach(title => {
+                        let a = document.createElement("a");
+                        a.innerText = "[nla.arc-" + title.pi + '] ' + title.name;
+                        a.href = pandasUrl + "/titles/" + title.id;
+                        div.appendChild(a);
+                        div.appendChild(document.createElement("br"));
+                    })
+                }
+                let div2 = document.createElement('div');
+                div2.classList.add('pandas-div');
+                div2.innerHTML = '';
+                div2.appendChild(div);
+                let container = link.parentNode;
+                if (location.host === 'www.bing.com') {
+                    container.insertBefore(div2, link.nextSibling ? link.nextSibling.nextSibling : null);
+                } else {
+                    container.insertBefore(div2, container.childNodes[0].nextSibling);
+                }
+            });
+    })
+}
--- ui/src/pandas/discovery/BookmarkletController.java
+++ ui/src/pandas/discovery/BookmarkletController.java
@@ -1,12 +1,12 @@
 package pandas.discovery;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriUtils;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,7 +16,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class BookmarkletController {
     @GetMapping(value = "/bookmarklet")
     public void get(Model model, HttpServletRequest request) throws IOException {
-        try (InputStream stream = BookmarkletController.class.getResourceAsStream("/bookmarklets/google.js")) {
+        try (InputStream stream = BookmarkletController.class.getResourceAsStream("/bookmarklets/bookmarklet.js")) {
             String bookmarklet = new String(stream.readAllBytes(), UTF_8);
             String baseUrl = ServletUriComponentsBuilder.fromContextPath(request).build().toString().replaceFirst("/$", "");
             bookmarklet = bookmarklet.replace("https://pandas.nla.gov.au/admin", baseUrl);
