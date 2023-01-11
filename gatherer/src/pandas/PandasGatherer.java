77bbfa237fd555716d7a736b460be751139b7c82
Alex Osborne <aosborne@nla.gov.au>
2023-01-11 20:54:01 +0900

    EnableJpaAuditing in gatherer to bump instance modified date

--- gatherer/src/pandas/PandasGatherer.java
+++ gatherer/src/pandas/PandasGatherer.java
@@ -3,11 +3,13 @@ package pandas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PandasGatherer {
     public static void main(String[] args) {
         SpringApplication.run(PandasGatherer.class, args);
