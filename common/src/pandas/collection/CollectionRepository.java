340b38eb57795d462e890d3c37ce40883fc8d010
Alex Osborne <aosborne@nla.gov.au>
2023-09-21 15:24:38 +0900

    Use lowercase table names for better mariadb compat

--- common/src/pandas/collection/CollectionRepository.java
+++ common/src/pandas/collection/CollectionRepository.java
@@ -111,12 +111,12 @@ public interface CollectionRepository extends CrudRepository<Collection, Long> {
 
     @Query(value = """
         with recursive cte1(COL_ID, ROOT_COL_ID, TITLE_COUNT) as (
-            select cs.COL_ID, cs.COL_ID, (select count(*) from TITLE_COL tc where tc.COLLECTION_ID = cs.COL_ID) as TITLE_COUNT
-            from COL_SUBS cs
+            select cs.COL_ID, cs.COL_ID, (select count(*) from title_col tc where tc.COLLECTION_ID = cs.COL_ID) as TITLE_COUNT
+            from col_subs cs
             where cs.SUBJECT_ID = :subjectId
             union all
-            select c.COL_ID, d.ROOT_COL_ID, (select count(*) from TITLE_COL tc where tc.COLLECTION_ID = c.COL_ID) as TITLE_COUNT
-            from COL c
+            select c.COL_ID, d.ROOT_COL_ID, (select count(*) from title_col tc where tc.COLLECTION_ID = c.COL_ID) as TITLE_COUNT
+            from col c
             join cte1 d on c.COL_PARENT_ID = d.COL_ID)
         select ROOT_COL_ID as id, SUM(TITLE_COUNT) as titleCount
         from cte1
@@ -134,15 +134,15 @@ public interface CollectionRepository extends CrudRepository<Collection, Long> {
 
     @Query(value = """
                 with recursive descendents2(COL_ID, ROOT_COL_ID) as
-                         (select COL_ID, COL_ID from COL
+                         (select COL_ID, COL_ID from col
                           where COL_PARENT_ID = :collectionId
                           union all
                           select c.COL_ID, d.ROOT_COL_ID
-                          from COL c join descendents2 d on c.COL_PARENT_ID = d.COL_ID),
+                          from col c join descendents2 d on c.COL_PARENT_ID = d.COL_ID),
                      descendent_counts2(COL_ID, ROOT_COL_ID, TITLE_COUNT) as
                          (select d.COL_ID,
                                  d.ROOT_COL_ID,
-                                 (select count(*) from TITLE_COL tc where tc.COLLECTION_ID = d.COL_ID)
+                                 (select count(*) from title_col tc where tc.COLLECTION_ID = d.COL_ID)
                           from descendents2 d)
                 select ROOT_COL_ID as id, sum(TITLE_COUNT) as titleCount
                 from descendent_counts2
@@ -159,15 +159,15 @@ public interface CollectionRepository extends CrudRepository<Collection, Long> {
 
     @Query(value = """
         with recursive descendents3(COL_ID, ROOT_COL_ID) as
-                 (select COL_ID, COL_ID from COL
+                 (select COL_ID, COL_ID from col
                   where COL_ID in :collectionIds
                   union all
                   select c.COL_ID, d.ROOT_COL_ID
-                  from COL c join descendents3 d on c.COL_PARENT_ID = d.COL_ID),
+                  from col c join descendents3 d on c.COL_PARENT_ID = d.COL_ID),
              descendent_counts3(COL_ID, ROOT_COL_ID, TITLE_COUNT) as
                  (select d.COL_ID,
                          d.ROOT_COL_ID,
-                         (select count(*) from TITLE_COL tc where tc.COLLECTION_ID = d.COL_ID)
+                         (select count(*) from title_col tc where tc.COLLECTION_ID = d.COL_ID)
                   from descendents3 d)
         select ROOT_COL_ID as id, sum(TITLE_COUNT) as titleCount
         from descendent_counts3
@@ -199,17 +199,17 @@ public interface CollectionRepository extends CrudRepository<Collection, Long> {
 
     @Query(value = """
          with recursive descendents1(COL_ID, ROOT_COL_ID, START_DATE, END_DATE) as
-                         (select COL_ID, COL_ID, START_DATE, END_DATE from COL
+                         (select COL_ID, COL_ID, START_DATE, END_DATE from col
                           where COL_ID = :collectionId
                           union all
                           select c.COL_ID, d.ROOT_COL_ID, COALESCE(c.START_DATE, d.START_DATE), COALESCE(c.END_DATE, d.END_DATE)
-                          from COL c
+                          from col c
                           join descendents1 d on c.COL_PARENT_ID = d.COL_ID),
                 instanceIds(INSTANCE_ID) as
                     (select distinct i.INSTANCE_ID
                      from descendents1 d
-                     join TITLE_COL tc on tc.COLLECTION_ID = d.COL_ID
-                     join INSTANCE i on tc.TITLE_ID = i.TITLE_ID
+                     join title_col tc on tc.COLLECTION_ID = d.COL_ID
+                     join instance i on tc.TITLE_ID = i.TITLE_ID
                      where i.CURRENT_STATE_ID = 1
                        and (d.START_DATE is null or i.INSTANCE_DATE >= d.START_DATE)
                        and (d.END_DATE is null or i.INSTANCE_DATE <= d.END_DATE))
@@ -218,8 +218,8 @@ public interface CollectionRepository extends CrudRepository<Collection, Long> {
                        coalesce(sum(ig.GATHER_SIZE), 0) as gatheredBytes,
                        coalesce(sum(ig.GATHER_FILES), 0) as gatheredFiles
                 from instanceIds i2
-                join INSTANCE i on i.INSTANCE_ID = i2.INSTANCE_ID
-                join INS_GATHER ig on i.INSTANCE_ID = ig.INSTANCE_ID""",
+                join instance i on i.INSTANCE_ID = i2.INSTANCE_ID
+                join ins_gather ig on i.INSTANCE_ID = ig.INSTANCE_ID""",
             nativeQuery = true, queryRewriter = WithRecursiveQueryRewriter.class)
     CollectionStats calculateCollectionStats(long collectionId);
 }
