206171cdfb2de4ec68d7d90d8097efb58755873c
Alex Osborne <aosborne@nla.gov.au>
2024-01-09 16:57:33 +0900

    LoginController: Use ClientRegistrationRepository instead of InMemoryClientRegistrationRepository
    This should handle the case of SupplierClientRegistrationRepository.

--- ui/src/pandas/core/LoginController.java
+++ ui/src/pandas/core/LoginController.java
@@ -3,6 +3,7 @@ package pandas.core;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +16,23 @@ import java.util.Comparator;
 
 @Controller
 @ConditionalOnProperty("OIDC_URL")
-@ConditionalOnBean(InMemoryClientRegistrationRepository.class)
 public class LoginController {
-    private final InMemoryClientRegistrationRepository clientRegistrationRepository;
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
-    public LoginController(InMemoryClientRegistrationRepository clientRegistrationRepository) {
+    public LoginController(ClientRegistrationRepository clientRegistrationRepository) {
         this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
     @GetMapping("/login")
     public String login(Model model) {
         var registrations = new ArrayList<ClientRegistration>();
-        clientRegistrationRepository.forEach(registrations::add);
+        if (!(clientRegistrationRepository instanceof Iterable)) {
+            throw new IllegalStateException("clientRegistrationRepository " +
+                                               (clientRegistrationRepository.getClass()) +
+                                               " is not iterable");
+        }
+        var iterable = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        iterable.forEach(registrations::add);
         Collections.sort(registrations, Comparator.comparing(ClientRegistration::getRegistrationId));
         model.addAttribute("registrations", registrations);
         return "Login";
