1e6bdd96c6f7e716cdedbbf4f4bb9d52699858a6
Alex Osborne <aosborne@nla.gov.au>
2024-05-02 10:12:20 +0900

    LoginController: Add @ConditionalOnBean(ClientRegistrationRepository)
    This ensures it's not loaded in the unit tests which don't have this
    bean. Even though OIDC_URL might be defined in the environment.

--- ui/src/pandas/core/LoginController.java
+++ ui/src/pandas/core/LoginController.java
@@ -1,5 +1,6 @@
 package pandas.core;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -21,6 +22,7 @@ import java.util.Comparator;
 
 @Controller
 @ConditionalOnProperty("OIDC_URL")
+@ConditionalOnBean(ClientRegistrationRepository.class)
 public class LoginController {
     private final ClientRegistrationRepository clientRegistrationRepository;
 
