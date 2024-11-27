18f6655693756c8fe722d0bedbb5736ac132be5d
Alex Osborne <aosborne@nla.gov.au>
2024-11-27 22:42:22 +0900

    social: Workaround oauth compile error after Spring update

--- social/src/pandas/PandasOAuthConfig.java
+++ social/src/pandas/PandasOAuthConfig.java
@@ -3,7 +3,6 @@ package pandas;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesRegistrationAdapter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +11,6 @@ import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2A
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-
-import java.util.ArrayList;
 
 /**
  * Loads application-oauth.properties only if OIDC_URL is defined.
@@ -27,13 +23,15 @@ import java.util.ArrayList;
 public class PandasOAuthConfig {
 
     // For non-webapp mode. Spring Boot autoconfigures this in webapp mode.
-    @Bean
-    @ConditionalOnMissingBean(ClientRegistrationRepository.class)
-    InMemoryClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties properties) {
-        var registrations = new ArrayList<>(
-                OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties).values());
-        return new InMemoryClientRegistrationRepository(registrations);
-    }
+// FIXME: broken in spring boot 3.4.0, we're not currently this so just commenting out for now
+//
+//    @Bean
+//    @ConditionalOnMissingBean(ClientRegistrationRepository.class)
+//    InMemoryClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties properties) {
+//        var registrations = new ArrayList<>(
+//                OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties).values());
+//        return new InMemoryClientRegistrationRepository(registrations);
+//    }
 
     // For non-webapp mode. Spring Boot autoconfigures this in webapp mode.
     @Bean
